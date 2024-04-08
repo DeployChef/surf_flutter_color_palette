02ffd39af254437318e48d5a2c708433965f5128
DeployChef <dc.titovvlad@gmail.com>
2024-04-08 20:32:14 +0300

    refactoring repo

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:surf_flutter_courses_template/data_access/i_color_repository.dart';
 import 'package:surf_flutter_courses_template/data_access/mock_color_repository.dart';
 import 'package:surf_flutter_courses_template/screens/palette_screen.dart';
 import 'package:surf_flutter_courses_template/themes.dart';
 
 void main() {
-  runApp(const MainApp());
+  final repo = MockColorRepository();
+
+  runApp(MainApp(repo: repo));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final IColorRepository _repo;
+
+  const MainApp({super.key, required IColorRepository repo}) : _repo = repo;
 
   @override
   Widget build(BuildContext context) {
-    var repo = MockColorRepository();
-
     return MaterialApp(
       theme: AppTheme.lightTheme,
       home: PaletteScreen(
-        repository: repo,
+        repository: _repo,
       ),
     );
   }
